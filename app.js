b843af9b07e7e65d8a0e86d6f9bdb01432f33df1
Bipin Yadav <86704062+bipin7yadav@users.noreply.github.com>
2021-11-27 09:46:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var translate = document.querySelector("#translate");
+var txtinput = document.querySelector("#txt-input");
+var results = document.querySelector("#result");
+
+var server = "https://api.funtranslations.com/translate/shakespeare.json"
+
+function getTranslationURL(input){
+    return server + "?" + "text=" + input
+}
+
+function errorHandler(error){
+    console.log("error occured",error);
+    alert("something is wrong")
+}
+
+
+
+function clickHandler(){
+    var inputtext = txtinput.value
+
+    fetch(getTranslationURL(inputtext))
+
+    .then (response=>response.json())
+    .then (json =>{
+        var translatedText=json.contents.translated;
+        results.innerText = translatedText;
+    })
+    .catch(errorHandler)
+};
+translate.addEventListener("click", clickHandler)
